f70cc55cfc501a6fa4dfc0ab58ebc03333cbf9d8
Troy <troyjmcl85@gmail.com>
2019-12-13 20:38:55 -0700

    Changed img csv writer to fs.stream as it is faster than csv-writer and more memeoy friendly

--- db/datageneration/img.js
+++ db/datageneration/img.js
@@ -1,69 +1,56 @@
-var faker = require('faker');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-
-const csvWriter = createCsvWriter({
-  path: '././img.csv',
-  header: [
-    {id: 'img_small', title: 'img_small'},
-    {id: 'img_large', title: 'img_large'},
-    {id: 'img_zoom', title: 'img_zoom'},
-    {id: 'product_id', title: 'product_id'}
-  ]
-})
+const fs = require('fs');
 
 
 var productImages = function(imgCnt, id) {
   //Variable for set of S3 stored images
   var imageList = [
-    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64.jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500.jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600.jpg'},
-    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(1).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(1).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(1).jpg'},
-    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(2).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(2).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(2).jpg'},
-    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(3).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(3).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(3).jpg'},
-    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(4).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(4).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(4).jpg'},
-    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(5).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(5).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(5).jpg'},
-    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(6).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(6).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(6).jpg'},
-    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(7).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l300+(1).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(7).jpg'},
-    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(8).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(2).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(8).jpg'},
-    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(9).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(3).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(9).jpg'},
-    {img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(10).jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(4).jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(10).jpg'}
+    'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64.jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500.jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600.jpg,',
+    'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(1).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(1).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(1).jpg,',
+    'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(2).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(2).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(2).jpg,',
+    'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(3).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(3).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(3).jpg,',
+    'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(4).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(4).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(4).jpg,',
+    'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(5).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(5).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(5).jpg,',
+    'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(6).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(6).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(6).jpg,',
+    'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(7).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l300+(1).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(7).jpg,',
+    'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(8).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(2).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(8).jpg,',
+    'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(9).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(3).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(9).jpg,',
+    'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64+(10).jp,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500+(4).jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600+(10).jpg,',
   ];
 
   //Specific data for product #1 that is coordinated between projects
-  var prodOneList = [
-    {product_id: 1, img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64_chew1.jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500_chew1.jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600_chew1.jpg'},
-    {product_id: 1, img_small: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64_chew2.jpg', img_large: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500_chew2.jpg', img_zoom: 'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600_chew2.jpg'}
-  ];
-
-  var result;
-
+  var prodOneList =
+    'https://fec-product-images.s3.us-east-2.amazonaws.com/s-l64_chew1.jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500_chew1.jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600_chew1.jpg,1\nhttps://fec-product-images.s3.us-east-2.amazonaws.com/s-l64_chew2.jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l500_chew2.jpg,https://fec-product-images.s3.us-east-2.amazonaws.com/s-l1600_chew2.jpg,1'
+  ;
+  var result = "";
   if (id === 1) {
-    result = prodOneList;
+    result += prodOneList + '\n';
   } else {
-    result = imageList.slice(0, imgCnt);
+    let images = imageList.slice(0, imgCnt);
     for (var i = 0; i < imgCnt; i++) {
-      result[i]['product_id'] = id;
+      result += images[i] + id + '\n';
     }
   }
-  // console.log(result);
   return result
 };
 
 const generateImgData = () => {
+  let writeStream = fs.createWriteStream('img.csv');
   var imgCnt;
-  var insertArr = [];
-  for (var i = 1; i <= 100000; i++) {
+  var csvStr = 'img_small,img_large,img_zoom,product_id\n';
+  for (var i = 1; i <= 1000000; i++) {
     //Select random amount of images for product
     imgCnt = Math.floor(Math.random() * 8) + 1;
-    let oneProdRecords = productImages(imgCnt, i);
-    oneProdRecords.forEach(record => {
-      insertArr.push(record)
-    })
-    if(i % 2500 === 0) {
-      csvWriter.writeRecords(insertArr)
-        .catch((err) => console.log(err))
-      insertArr = []
+    csvStr += productImages(imgCnt, i);
+
+    if(i % 10000 === 0) {
+      writeStream.write(csvStr)
+      csvStr = '';
     }
   }
-  console.log('img data generated')
-};
+  writeStream.on('finish', () => {
+    console.log('wrote all data to file')
+  })
+  console.log('img data finished')
+
+}
 generateImgData()
--- db/datageneration/prod.js
+++ db/datageneration/prod.js
@@ -10,16 +10,15 @@ const csvWriter = createCsvWriter({
 
 let generateProdData = () => {
   var fakeProducts = [];
-  for (var i = 0; i <= 100000; i++) {
+  for (var i = 0; i <= 1000000; i++) {
     if (i === 0) {
       fakeProducts.push({name: 'Star Wars Super Deluxe 24" Talking Plush: Chewbacca'});
     } else {
       fakeProducts.push({name: faker.random.words(3)});
     }
-    if(i % 2500 === 0) {
+    if(i % 10000 === 0) {
       csvWriter.writeRecords(fakeProducts)
       .catch((err) => console.log(err))
-      fakeProducts = []
     }
   }
   console.log('product data generated')
