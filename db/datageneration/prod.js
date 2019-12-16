b9f64880fd68552fe509ef39480d7424399daa49
Troy <troyjmcl85@gmail.com>
2019-12-16 11:28:55 -0700

    Trying to get seeds to work for large files, tried adding pg-copy-streams module, still erroring out and not writing to db

--- db/datageneration/prod.js
+++ db/datageneration/prod.js
@@ -1,29 +1,53 @@
 const faker = require('faker');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const fs = require('fs');
 
-const csvWriter = createCsvWriter({
-  path: '././product.csv',
-  header: [
-    {id: 'name', title: 'name'}
-  ]
-})
 
-let generateProdData = () => {
-  var fakeProducts = [];
-  for (var i = 0; i <= 1000000; i++) {
+const generateProdData = () => {
+  let writeStream = fs.createWriteStream('product.csv')
+  var fakeProducts = "name\n";
+  for (var i = 1; i <= 50000; i++) {
     if (i === 0) {
-      fakeProducts.push({name: 'Star Wars Super Deluxe 24" Talking Plush: Chewbacca'});
+      fakeProducts +='Star Wars Super Deluxe 24" Talking Plush: Chewbacca\n';
     } else {
-      fakeProducts.push({name: faker.random.words(3)});
+      fakeProducts += faker.lorem.words(3) + '\n';
     }
-    if(i % 10000 === 0) {
-      csvWriter.writeRecords(fakeProducts)
-      .catch((err) => console.log(err))
+    if ( i % 1000=== 0) {
+      writeStream.write(fakeProducts)
+      fakeProducts = "";
     }
   }
+  writeStream.on('finish', () => {
+    console.log('wrote all data to file')
+  })
   console.log('product data generated')
-
 }
-
 generateProdData()
+
+// const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+// const csvWriter = createCsvWriter({
+//   path: '././product.csv',
+//   header: [
+//     {id: 'name', title: 'name'}
+//   ]
+// })
+
+// let generateProdData = () => {
+//   var fakeProducts = [];
+//   for (var i = 0; i <= 1000; i++) {
+//     if (i === 0) {
+//       fakeProducts.push({name: 'Star Wars Super Deluxe 24" Talking Plush: Chewbacca'});
+//     } else {
+//       fakeProducts.push({name: faker.random.words(3)});
+//     }
+//     if(i % 10 === 0) {
+//       csvWriter.writeRecords(fakeProducts)
+//       .catch((err) => console.log(err))
+//     }
+//   }
+//   console.log('product data generated')
+
+// }
+
+// generateProdData()
 
